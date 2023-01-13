aebf9f608d697b3471a215454cb4a5f3904df8ad
Annie Pulzone <ajpulzone@gmail.com>
2023-01-13 10:47:10 -0800

    rending the json expected, and the request is testd. Running out of time to to write the other tests

--- app/facades/tourist_sights_facade.rb
+++ app/facades/tourist_sights_facade.rb
@@ -3,7 +3,8 @@ class TouristSightsFacade
   def self.tourist_sights(country)
     capital_info = CountryFacade.capital_info(country)
     sights = TouristSightsService.tourist_sights(capital_info.longitude, capital_info.latitude)
-    sights[:features].map do |sight|
+    limit_20 = sights[:features][0..20]
+    limit_20.map do |sight|
       TouristSight.new(sight)
     end 
   end
