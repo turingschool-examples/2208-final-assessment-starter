223b1ab48a5445f0f5ed9d9485de10e84fdd65c0
Amanda Ross <amikaross@gmail.com>
2023-01-13 09:24:24 -0700

    Add cassettes to all existing tests

--- spec/facades/country_facade_spec.rb
+++ spec/facades/country_facade_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe CountryFacade do 
     it '.capital_info' do 
-        capital = CountryFacade.capital_info("Nigeria")
-        expect(capital).to be_an_instance_of(Capital)
+        VCR.use_cassette('facade_test_capital_info') do
+            capital = CountryFacade.capital_info("Nigeria")
+            expect(capital).to be_an_instance_of(Capital)
+        end
     end 
 end 
--- spec/requests/get_tourist_sights_spec.rb
+++ spec/requests/get_tourist_sights_spec.rb
@@ -2,7 +2,30 @@ require 'rails_helper'
 
 RSpec.describe 'Get Tourist Sights' do 
   it 'returns a list of tourist_sight objects with name, formatted, address, and place_id' do 
-    get "/api/v1/tourist_sights?country=France"
-    
+    VCR.use_cassette('get_tourist_sights_for_france') do 
+      get "/api/v1/tourist_sights?country=France"
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+
+      response_body = JSON.parse(response.body, symbolize_names: true)
+  
+      sights = response_body[:data]
+  
+      expect(sights).to be_an(Array)
+  
+      sights.each do |sight|
+        expect(sight).to be_a(Hash)
+        expect(sight[:id]).to eq(nil)
+        expect(sight[:type]).to eq("tourist_sight")
+        expect(sight[:attributes]).to be_a(Hash)
+        expect(sight[:attributes]).to have_key(:name)
+        expect(sight[:attributes][:name]).to be_a(String)
+        expect(sight[:attributes]).to have_key(:address)
+        expect(sight[:attributes][:address]).to be_a(String)
+        expect(sight[:attributes]).to have_key(:place_id)
+        expect(sight[:attributes][:place_id]).to be_a(String)
+      end
+    end
   end
 end
