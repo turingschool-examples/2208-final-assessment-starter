6be76ab7fe4e359dbc66a4c3cb827b042333a1c3
Lucas Colwell <colwellc37@gmail.com>
2023-01-13 10:02:38 -0700

    Feat: update place class to tourist sight

--- app/facades/places_facade.rb
+++ app/facades/places_facade.rb
@@ -2,7 +2,7 @@ class PlacesFacade
   def self.places(capital_info)
     places = PlacesService.get_tourist_sights(capital_info)
     locations = places.map do |p|
-      Place.new(p)
+      TouristSight.new(p)
     end
   end
 end
