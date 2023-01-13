7ac18e32cb800a527f1112dee9459e8fbc0981d1
Emily Port <emilylport@gmail.com>
2023-01-13 11:17:55 -0700

    add memoization to capitals controller to reduce api calls

--- app/facades/tourist_sites_facade.rb
+++ app/facades/tourist_sites_facade.rb
@@ -2,7 +2,6 @@ class TouristSitesFacade
   def self.tourist_sites(country)
     near_country = CountryFacade.capital_info(country)
     if near_country != nil 
-
       long = near_country.longitude
       lat = near_country.latitude
       TouristSitesService.nearby_tourist_sites(long, lat)[:features].map do |site|
