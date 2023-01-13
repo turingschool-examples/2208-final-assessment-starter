4b378ee087e5e14eee34e8e21d5ff4ae8f0b051b
Sage Skaff <skaffsage@gmail.com>
2023-01-13 09:54:25 -0800

    Update get tourist sights request test

--- spec/requests/get_tourist_sights_spec.rb
+++ spec/requests/get_tourist_sights_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Get Tourist Sights' do 
-    it 'returns capital info as json' do 
+    it 'returns tourist sights as json' do 
         country = "France"
 
         get "/api/v1/tourist_sights?country=#{country}"
@@ -9,7 +9,13 @@ RSpec.describe 'Get Tourist Sights' do
         expect(response).to be_successful
 
         parsed_response = JSON.parse(response.body,symbolize_names: true)
-
         expect(parsed_response).to be_a(Hash)
+        expect(parsed_response).to have_key(:data)
+        expect(parsed_response[:data].first).to have_key(:id)
+        expect(parsed_response[:data].first).to have_key(:type)
+        expect(parsed_response[:data].first).to have_key(:attributes)
+        expect(parsed_response[:data].first[:attributes]).to have_key(:name)
+        expect(parsed_response[:data].first[:attributes]).to have_key(:address)
+        expect(parsed_response[:data].first[:attributes]).to have_key(:place_id)
     end 
 end 
