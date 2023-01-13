16b1a8cd1eb0e21fc4a22a4d4a4b3d5dd0537496
Astrid Hecht <silentaviary@gmail.com>
2023-01-13 12:22:12 -0700

    fix: add first line in poro address

--- app/poro/tourist_sight.rb
+++ app/poro/tourist_sight.rb
@@ -3,7 +3,7 @@ class TouristSight
 
   def initialize(place_info)
     @name = place_info[:name]
-    @address = place_info[:address_line2]
+    @address = "#{place_info[:address_line1]}, #{place_info[:address_line2]}"
     @place_id = place_info[:place_id]
   end
 end
--- spec/facades/places_facade_spec.rb
+++ spec/facades/places_facade_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe PlacesFacade do
   it '.tourist_sights' do
     capital = CountryFacade.capital_info('France')
     sights = PlacesFacade.tourist_sights(capital.latitude, capital.longitude)
-    # binding.pry
+
     expect(sights).to be_an(Array)
     expect(sights.first).to be_an_instance_of(TouristSight)
   end
