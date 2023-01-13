2f6d749e1189d2d3c032e2f5f57de6a7f06a9b4e
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-01-13 11:40:31 -0700

    Update serializer name for correct response, update get request testing

--- app/controllers/api/v1/sights_controller.rb
+++ app/controllers/api/v1/sights_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::SightsController < ApplicationController
   def index
     country = params[:country]
     tourist_sights = CountryFacade.tourist_sights(country)
-    render json: TouristSightsSerializer.new(tourist_sights)
+    render json: TouristSightSerializer.new(tourist_sights)
   end
 end
--- spec/requests/get_tourist_sights_spec.rb
+++ spec/requests/get_tourist_sights_spec.rb
@@ -2,21 +2,24 @@ require 'rails_helper'
 
 RSpec.describe 'Get Tourist Sights' do 
   it 'returns capital info as json' do 
-      country = "France"
+    country = "France"
 
-      get "/api/v1/tourist_sights?country=#{country}"
+    get "/api/v1/tourist_sights?country=#{country}"
 
-      expect(response).to be_successful
+    expect(response).to be_successful
 
-      parsed_response = JSON.parse(response.body,symbolize_names: true)
-      require 'pry'; binding.pry
-      expect(parsed_response).to be_a(Hash)
-      expect(parsed_response).to have_key(:data)
-      expect(parsed_response[:data]).to have_key(:id)
-      expect(parsed_response[:data]).to have_key(:type)
-      expect(parsed_response[:data]).to have_key(:attributes)
-      expect(parsed_response[:data][:attributes]).to have_key(:name)
-      expect(parsed_response[:data][:attributes]).to have_key(:address)
-      expect(parsed_response[:data][:attributes]).to have_key(:place_id)
+    sights = JSON.parse(response.body,symbolize_names: true)
+
+    expect(sights).to be_a(Hash)
+    expect(sights).to have_key(:data)
+
+    sight = sights[:data].first
+
+    expect(sight).to have_key(:id)
+    expect(sight).to have_key(:type)
+    expect(sight).to have_key(:attributes)
+    expect(sight[:attributes]).to have_key(:name)
+    expect(sight[:attributes]).to have_key(:address)
+    expect(sight[:attributes]).to have_key(:place_id)
   end 
 end 
