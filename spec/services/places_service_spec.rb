2485460c4eda3c826d163a27f909f02167d3a186
Lucas Colwell <colwellc37@gmail.com>
2023-01-13 09:25:00 -0700

    Test: test places service

---
+++ spec/services/places_service_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe PlacesService, :vcr do
+  it '.get_places' do
+    capital = CountryFacade.capital_info("France")
+    places = PlacesService.get_hotels(capital)
+    places.each do |place|
+      expect(place.keys).to eq([:type, :properties, :geometry])
+      expect(place[:properties]).to have_key(:name)
+      expect(place[:properties]).to have_key(:formatted)
+      expect(place[:properties]).to have_key(:place_id)
+      expect(place[:properties][:name]).to be_a String
+      expect(place[:properties][:formatted]).to be_a String
+      expect(place[:properties][:place_id]).to be_a String
+    end
+  end
+end
