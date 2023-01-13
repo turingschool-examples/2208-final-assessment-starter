67b3afc0cee1c9ae759695324573cc2b14463bab
Lucas Colwell <colwellc37@gmail.com>
2023-01-13 10:01:59 -0700

    Test: update place class to TouristSight class

--- spec/facades/place_facade_spec.rb
+++ spec/facades/place_facade_spec.rb
@@ -3,9 +3,11 @@ require 'rails_helper'
 RSpec.describe PlacesFacade, :vcr do
   it 'returns places from capital of searched country' do
     capital_info = CountryFacade.capital_info("France")
-    places = PlacesFacade.places(capital_info)
+    sights = PlacesFacade.places(capital_info)
 
-    expect(places).to be_an Array
-    expect(places.first).to be_a Place
+    expect(sights).to be_an Array
+    sights.each do |s|
+      expect(s).to be_a TouristSight
+    end
   end
 end
