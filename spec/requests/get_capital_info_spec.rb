93a695bea01b50ca67b485c8b8e422c2c05171c1
bkeener <bkeener7@gmail.com>
2023-01-13 07:19:12 -0700

    Configure initial setup

--- spec/requests/get_capital_info_spec.rb
+++ spec/requests/get_capital_info_spec.rb
@@ -1,24 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe 'Get Capital Info' do 
-    it 'returns capital info as json' do 
-        country = "France"
+RSpec.describe 'Get Capital Info' do
+  it 'returns capital info as json', :vcr do
+    country = 'France'
 
-        get "/api/v1/capital_info?country=#{country}"
+    get "/api/v1/capital_info?country=#{country}"
 
-        expect(response).to be_successful
+    expect(response).to be_successful
 
-        parsed_response = JSON.parse(response.body,symbolize_names: true)
+    parsed_response = JSON.parse(response.body, symbolize_names: true)
 
-        expect(parsed_response).to be_a(Hash)
-        expect(parsed_response).to have_key(:data)
-        expect(parsed_response[:data]).to have_key(:id)
-        expect(parsed_response[:data]).to have_key(:type)
-        expect(parsed_response[:data]).to have_key(:attributes)
-        expect(parsed_response[:data][:attributes]).to have_key(:city)
-        expect(parsed_response[:data][:attributes]).to have_key(:country)
-        expect(parsed_response[:data][:attributes]).to have_key(:country_code)
-        expect(parsed_response[:data][:attributes]).to have_key(:latitude)
-        expect(parsed_response[:data][:attributes]).to have_key(:longitude)
-    end 
-end 
+    expect(parsed_response).to be_a(Hash)
+    expect(parsed_response).to have_key(:data)
+    expect(parsed_response[:data]).to have_key(:id)
+    expect(parsed_response[:data]).to have_key(:type)
+    expect(parsed_response[:data]).to have_key(:attributes)
+    expect(parsed_response[:data][:attributes]).to have_key(:city)
+    expect(parsed_response[:data][:attributes]).to have_key(:country)
+    expect(parsed_response[:data][:attributes]).to have_key(:country_code)
+    expect(parsed_response[:data][:attributes]).to have_key(:latitude)
+    expect(parsed_response[:data][:attributes]).to have_key(:longitude)
+  end
+end
--- spec/services/countries_service_spec.rb
+++ spec/services/countries_service_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe CountriesNowService do 
-    it '.capital_info' do 
-        response = CountriesNowService.get_capital_city("France")
+RSpec.describe CountriesNowService do
+  it '.capital_info', :vcr do
+    response = CountriesNowService.get_capital_city('France')
 
-        expect(response).to be_a Hash
-        expect(response[:error]).to eq false
-        expect(response[:data]).to have_key :name
-        expect(response[:data]).to have_key :capital
-    end 
-end 
+    expect(response).to be_a Hash
+    expect(response[:error]).to eq false
+    expect(response[:data]).to have_key :name
+    expect(response[:data]).to have_key :capital
+  end
+end
