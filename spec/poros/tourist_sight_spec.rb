2a92a12f9c3e0f9ee546846c83d51dba83ed0f32
Madeline Mauser <madeline.mauser@gmail.com>
2023-01-13 09:53:25 -0700

    Add tourist sight poro spec

---
+++ spec/poros/tourist_sight_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe TouristSight do 
+    it 'exists and has attributes' do 
+        tourist_sight_info = {
+                            :name=>"Tour de l'horloge", 
+                            :address=>"Tour de l'horloge, Allée de l'Horloge, 23200 Aubusson, France", 
+                            :place_id=>"515d1f5d627"
+                        }
+
+        sight = TouristSight.new(tourist_sight_info)
+
+        expect(sight).to be_an_instance_of(TouristSight)
+        expect(sight.name).to eq("Tour de l'horloge")
+        expect(sight.address).to eq("Tour de l'horloge, Allée de l'Horloge, 23200 Aubusson, France")
+        expect(sight.country_code).to eq("515d1f5d627")
+    end 
+end 
