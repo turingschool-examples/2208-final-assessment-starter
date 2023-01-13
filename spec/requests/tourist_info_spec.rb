b5a473820ab4e098604274b8006fd288ae592385
Shawn Lee <Shawnl93@hotmail.com>
2023-01-13 10:50:17 -0700

    request test finished

--- spec/requests/tourist_info_spec.rb
+++ spec/requests/tourist_info_spec.rb
@@ -10,5 +10,13 @@ RSpec.describe 'Get Capital Info' do
         info = JSON.parse(response.body,symbolize_names: true)
         
         expect(info).to be_a(Hash)
+        expect(info).to have_key(:data)
+        expect(info[:data][0]).to have_key(:id)
+        expect(info[:data][0]).to have_key(:type)
+        expect(info[:data][0]).to have_key(:attributes)
+        expect(info[:data][0][:attributes]).to have_key(:name)
+        expect(info[:data][0][:attributes]).to have_key(:address)
+        expect(info[:data][0][:attributes]).to have_key(:place_id)
+
     end
 end 
