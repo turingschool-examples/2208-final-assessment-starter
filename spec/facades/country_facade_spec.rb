c991365d0de538428e4bdcfb1cfd1e001496591f
Rich Kaht <adamselene1776@protonmail.com>
2023-01-13 11:34:36 -0700

    Fix: add error handling for Country Facade

--- spec/facades/country_facade_spec.rb
+++ spec/facades/country_facade_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe CountryFacade do
         capital = CountryFacade.capital_info("Nigeria")
         expect(capital).to be_an_instance_of(Capital)
     end 
+
+    it 'return nil if the country is not found' do
+      capital = CountryFacade.capital_info('Aslandia')
+      expect(capital).to eq(nil)
+    end
 end 
