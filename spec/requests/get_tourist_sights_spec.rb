b494155a2a88da63af08a0561217a9254dc59a69
Mackenzie Leng <kenz.joy@outlook.com>
2023-01-13 09:44:17 -0700

    routing

---
+++ spec/requests/get_tourist_sights_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe 'get toursit sights info' do
+  it 'returns tourist sight info as json' do
+    filter = 'circle:2.3200410217200766,48.8588897,20000'
+    limit = 20
+    
+
+  end
+end
