24a2cebdd767e199d536ba74b68b8dc7f8727f31
Shawn Lee <Shawnl93@hotmail.com>
2023-01-13 10:31:41 -0700

    commit

--- app/facades/sight_facade.rb
+++ app/facades/sight_facade.rb
@@ -2,7 +2,10 @@ class SightFacade
     def self.tourist(country)
        latitude = CountryFacade.capital_info(country).latitude
        longitude =CountryFacade.capital_info(country).longitude
-       map = "#{latitude},#{longitude}"
+       map = "#{longitude},#{latitude}"
         data = SightService.show_me(country, map)
+        data[:features].map do |info|
+            Sight.new(info)
+        end
     end
 end
---
+++ app/poro/sight.rb
@@ -0,0 +1,7 @@
+class Sight
+    def initialize(data)
+        @name = data[:properties][:name]
+        @address = data[:properties][:address_line1] + " " + data[:properties][:address_line2]
+        @place_id = data[:properties][:place_id]
+    end
+end
