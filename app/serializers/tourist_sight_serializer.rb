b1a30e3d1c84a9873627b5dfbe5ed4da64846d4d
Naomi Yocum <naomiyocum24@gmail.com>
2023-01-13 10:40:31 -0700

    Complete assessment

--- app/serializers/tourist_sight_serializer.rb
+++ app/serializers/tourist_sight_serializer.rb
@@ -1,5 +1,6 @@
 class TouristSightSerializer
   include JSONAPI::Serializer
+
   set_id {nil}
   attributes :name, :place_id
   attribute :address do |obj|
--- spec/requests/get_tourist_sights_spec.rb
+++ spec/requests/get_tourist_sights_spec.rb
@@ -1,11 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe 'Get Tourist Sights' do
-  it 'returns the tourist sights as json' do
-    params = {country: 'France'}
-    get '/api/v1/tourist_sights', params: params
+  describe 'it returns tourist sights' do
+    it 'returns tourist sights' do
+      country = "France"
 
-    expect(response).to be_successful
-    response = JSON.parse(response.body, symbolize_names: true)
+      get "/api/v1/tourist_sights?country=#{country}"
+
+      expect(response).to be_successful
+
+      parsed_response = JSON.parse(response.body,symbolize_names: true)
+
+      expect(parsed_response).to be_a(Hash)
+      expect(parsed_response).to have_key(:data)
+      expect(parsed_response[:data][0]).to have_key(:id)
+      expect(parsed_response[:data][0]).to have_key(:type)
+      expect(parsed_response[:data][0]).to have_key(:attributes)
+      expect(parsed_response[:data][0][:attributes]).to have_key(:name)
+      expect(parsed_response[:data][0][:attributes]).to have_key(:address)
+      expect(parsed_response[:data][0][:attributes]).to have_key(:place_id)
+    end
   end
 end
