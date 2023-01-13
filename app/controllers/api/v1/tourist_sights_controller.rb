28adf7093d3ead909488323e7d507e8d9a82b14a
Rich Kaht <adamselene1776@protonmail.com>
2023-01-13 11:41:02 -0700

    Fix: Add error handling for tourist_sights endpoint

--- app/controllers/api/v1/tourist_sights_controller.rb
+++ app/controllers/api/v1/tourist_sights_controller.rb
@@ -1,9 +1,13 @@
 class Api::V1::TouristSightsController < ApplicationController
   def index
     capital = CountryFacade.capital_info(params[:country])
-    
-    tourist_sights = PlaceFacade.get_tourist_places(capital.longitude, capital.latitude)
-    # require 'pry'; binding.pry
-    render json: PlaceSerializer.serialize_places(tourist_sights).to_json
+
+    if capital.nil?
+      render json: {error: 'country not found'}, status: 404
+    else
+      tourist_sights = PlaceFacade.get_tourist_places(capital.longitude, capital.latitude)
+  
+      render json: PlaceSerializer.serialize_places(tourist_sights).to_json
+    end
   end
 end
--- spec/requests/get_tourist_sites_spec.rb
+++ spec/requests/get_tourist_sites_spec.rb
@@ -16,4 +16,13 @@ RSpec.describe 'GET tourist sites' do
     expect(parsed_json[:data][0][:attributes][:address]).to be_a String
     expect(parsed_json[:data][0][:attributes][:place_id]).to be_a String
   end
+
+  it 'returns a 404 error if the country is not found' do
+    get '/api/v1/tourist_sights?country=Aslandia'
+
+    expect(response).to have_http_status(404)
+    parsed_json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed_json[:error]).to eq('country not found')
+  end
 end
