a1dfc41ffa5526f156416c7b4d972b22aebee4e3
Lucas Colwell <colwellc37@gmail.com>
2023-01-13 11:03:33 -0700

    Feat: edge case england

--- app/controllers/api/v1/capitals_controller.rb
+++ app/controllers/api/v1/capitals_controller.rb
@@ -1,7 +1,11 @@
-class Api::V1::CapitalsController < ApplicationController 
-    def show 
-        country = params[:country]
+class Api::V1::CapitalsController < ApplicationController
+    def show
+        if params[:country].downcase != "england"
+          country = params[:country]
+        else
+          country = "United Kingdom"
+        end
         capital = CountryFacade.capital_info(country)
         render json: CapitalSerializer.new(capital)
-    end 
-end 
+    end
+end
