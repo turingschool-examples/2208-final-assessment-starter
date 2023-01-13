bea6e1eb62bb26be992d240ea806d26eadc93f18
Mostafa Sakr <msakr3@gmail.com>
2023-01-13 10:12:03 -0700

    Add test and implementation code for tourist_spot_facade.

---
+++ app/facades/tourist_spot_facade.rb
@@ -0,0 +1,10 @@
+class TouristSpotFacade
+  def self.spots_info(country)
+      capital_city = CountriesNowService.get_capital_city(country)[:data][:capital]
+      capital_info = GeocodingService.capital_info(capital_city,country).first
+      capital = Capital.new(capital_info, country)
+      PlacesService.get_tourist_spots(capital.longitude, capital.latitude)[:features].map do |place|
+        TouristSpot.new(place)
+      end
+  end 
+end 
