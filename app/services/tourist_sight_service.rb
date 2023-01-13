b9c7c4acb55f330b2623c7e5ffe9cd8eb50cf38c
Will Wang <willy.j.wang@gmail.com>
2023-01-13 11:58:17 -0600

    Refactor service and facade

--- app/services/tourist_sight_service.rb
+++ app/services/tourist_sight_service.rb
@@ -1,20 +1,10 @@
 class TouristSightService
   def self.get_url(url)
-    # response = conn.get('/v2/places') do |req|
-    #   req.params
-    # end
     JSON.parse(conn.get(url).body, symbolize_names: true)
   end
 
   def self.get_tourist_sights(capital_coords)
-    # require "pry"; binding.pry
-    response = conn.get('/v2/places') do |req|
-      req.params["filter"] = "circle:#{capital_coords}"
-    end
-    # require "pry"; binding.pry
-    test = JSON.parse(response.body, symbolize_names: true)[:features]
-
-    # get_url("/v2/places?filter=circle:#{capital_coords}")
+    get_url("/v2/places?filter=circle:#{capital_coords}")[:features]
   end
 
   def self.conn
@@ -24,5 +14,3 @@ class TouristSightService
     end
   end
 end
-
-# https://api.geoapify.com/v2/places?categories=tourism.sights&filter=circle:-0.07071648508463113,51.50848194136378,2000&apiKey=7dc9a5c49282409f9e238e986d8e1b36
--- spec/requests/get_tourist_sights_spec.rb
+++ spec/requests/get_tourist_sights_spec.rb
@@ -9,7 +9,6 @@ RSpec.describe 'Get Tourist Sights' do
         expect(response).to be_successful
         parsed_response = JSON.parse(response.body,symbolize_names: true)
 
-        require "pry"; binding.pry
         expect(parsed_response).to be_a(Hash)
         expect(parsed_response).to have_key(:data)
         expect(parsed_response[:data]).to be_an(Array)
@@ -23,4 +22,27 @@ RSpec.describe 'Get Tourist Sights' do
         expect(tourist_sight[:attributes]).to have_key(:address)
         expect(tourist_sight[:attributes]).to have_key(:place_id)
     end
+    #Commented out second test b/c it takes long enough for one country
+    # it 'can return the tourist sights of another country as a json' do
+    #   country = "France"
+    #
+    #   get "/api/v1/tourist_sights?country=#{country}"
+    #
+    #   expect(response).to be_successful
+    #   parsed_response = JSON.parse(response.body,symbolize_names: true)
+    #
+    #   # require "pry"; binding.pry
+    #   expect(parsed_response).to be_a(Hash)
+    #   expect(parsed_response).to have_key(:data)
+    #   expect(parsed_response[:data]).to be_an(Array)
+    #
+    #   tourist_sight = parsed_response[:data][0]
+    #
+    #   expect(tourist_sight).to have_key(:id)
+    #   expect(tourist_sight).to have_key(:type)
+    #   expect(tourist_sight).to have_key(:attributes)
+    #   expect(tourist_sight[:attributes]).to have_key(:name)
+    #   expect(tourist_sight[:attributes]).to have_key(:address)
+    #   expect(tourist_sight[:attributes]).to have_key(:place_id)
+    # end
 end
