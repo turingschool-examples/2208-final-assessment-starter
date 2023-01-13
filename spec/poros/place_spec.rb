3d815be2e3aec19bed2ce915aeb0f348b084cda7
Astrid Hecht <silentaviary@gmail.com>
2023-01-13 09:57:46 -0700

    test: write Place poro test

---
+++ spec/poros/place_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+RSpec.describe Place do
+  it 'exists and has attributes' do
+    place_info = {
+      "name": 'U.S. bicentennial commemoration/time capsule',
+      "country": 'United States',
+      "country_code": 'us',
+      "state": 'Colorado',
+      "county": 'Boulder County',
+      "city": 'Boulder',
+      "postcode": '80306',
+      "suburb": 'Pearl Street Mall',
+      "street": 'Pearl Street Mall',
+      "lon": -105.27805266847703,
+      "lat": 40.01833035,
+      "state_code": 'CO',
+      "formatted": 'U.S. bicentennial commemoration/time capsule, Pearl Street Mall, Boulder, CO 80306, United States of America',
+      "address_line1": 'U.S. bicentennial commemoration/time capsule',
+      "address_line2": 'Pearl Street Mall, Boulder, CO 80306, United States of America',
+      "categories": [
+        'tourism',
+        'tourism.sights',
+        'tourism.sights.memorial'
+      ],
+      "details": [
+        'details.historic'
+      ],
+      "datasource": {
+        "sourcename": 'openstreetmap',
+        "attribution": '© OpenStreetMap contributors',
+        "license": 'Open Database Licence',
+        "url": 'https://www.openstreetmap.org/copyright',
+        "raw": {
+          "name": 'U.S. bicentennial commemoration/time capsule',
+          "osm_id": 621_297_736,
+          "historic": 'memorial',
+          "osm_type": 'w',
+          "historic_1": 'time capsule',
+          "inscription": 'August 6, 1977 Beneath this boulder lies a time capsule placed by the city of Boulder Centennial-Bicentennial Commission for the Boulder Centennial-Bicentennial Commission  Open the capsule July 4, 2075 City government Mayor Frank Buchanan …'
+        }
+      },
+      "distance": 716,
+      "place_id": '51c83a699dcb515ac059fffe20ae58024440f00102f901484008250000000092032c552e532e20626963656e74656e6e69616c20636f6d6d656d6f726174696f6e2f74696d652063617073756c65'
+    }
+
+    country = 'United States'
+
+    place = Place.new(place_info, country)
+
+    expect(place).to be_an_instance_of(Place)
+    expect(place.type).to eq('tourist_sight')
+    expect(place.attributes).to be_a Hash
+    expect(place.attributes[:name]).to eq('U.S. bicentennial commemoration/time capsule')
+    expect(place.attributes[:address]).to eq('Pearl Street Mall, Boulder, CO 80306, United States of America')
+    expect(place.attributes[:place_id]).to eq('51c83a699dcb515ac059fffe20ae58024440f00102f901484008250000000092032c552e532e20626963656e74656e6e69616c20636f6d6d656d6f726174696f6e2f74696d652063617073756c65')
+  end
+end
