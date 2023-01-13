c48cdae5bae2aa7341fdf3b198de912ea90f6a4f
Eli Fuchsman <efuchsman@gmail.com>
2023-01-13 10:33:38 -0700

    Complete request when records exist

--- spec/requests/get_tourist_sights_spec.rb
+++ spec/requests/get_tourist_sights_spec.rb
@@ -18,6 +18,29 @@ RSpec.describe "Tourist Sights API" do
 
         expect(response).to have_http_status(200)
       end
+
+      it "returns tourist sights" do
+        json = JSON.parse(response.body, symbolize_names: true)
+
+        expect(json).to be_a Hash
+
+        expect(json).to have_key :data
+        expect(json[:data]).to be_a Array
+
+        expect(json[:data].first).to have_key :id
+        expect(json[:data].first).to have_key :attributes
+
+        expect(json[:data].first[:id]).to be nil
+        expect(json[:data].first[:attributes]).to be_a Hash
+
+        expect(json[:data].first[:attributes]).to have_key :name
+        expect(json[:data].first[:attributes]).to have_key :address
+        expect(json[:data].first[:attributes]).to have_key :place_id
+
+        expect(json[:data].first[:attributes][:name]).to be_a(String).or(be(nil))
+        expect(json[:data].first[:attributes][:address]).to be_a String
+        expect(json[:data].first[:attributes][:place_id]).to be_a String
+      end
     end
   end
 end
